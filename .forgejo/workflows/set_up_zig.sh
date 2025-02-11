e2d7ff259c5a6532b96e97915c75345b0fa77652
Jora Troosh <tensorush@gmail.com>
2025-02-11 13:50:33 +0300

    fix: forgejo ci

--- .forgejo/workflows/set_up_zig.sh
+++ .forgejo/workflows/set_up_zig.sh
@@ -1,6 +1,4 @@
+# Temporary substitute for setup-zig Forgejo Action
 curl $(curl https://ziglang.org/download/index.json | jq -r '.master."x86_64-linux".tarball') -o zig.tar.xz
 tar -xf zig.tar.xz
 mv zig*/ zig/
-./zig/zig version
-chmod +x zig/zig
-echo "export PATH="$(pwd)/zig:\$PATH"" >> ~/.profile
