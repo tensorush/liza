fdf4df29edbfbc96c3dbad1f8e5a193f3a5fb2e0
Jora Troosh <tensorush@gmail.com>
2025-02-13 16:58:49 +0300

    fix: update forgejo ci

--- src/templates/.forgejo/workflows/set_up_zig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Temporary substitute for the future setup-zig Forgejo Action
-curl $(curl https://ziglang.org/download/index.json | jq -r '.master."x86_64-linux".tarball') -o zig.tar.xz
-tar -xf zig.tar.xz
-mv zig*/ zig/
-echo $(pwd)/zig >> $GITHUB_PATH
-echo $GITHUB_PATH
